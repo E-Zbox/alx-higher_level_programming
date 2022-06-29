a1562c654b5b954caada1dcdba7c66c96c80e906
E-Zbox <zboxstudios97@gmail.com>
2022-06-29 20:43:18 +0100

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for num in range(1, 101):
         if (num % 3 == 0):
             print("Fizz", end="")
-        
+
         if (num % 5 == 0):
             print("Buzz", end="")
 
