fd7e8c78150adefd55eecadd8ff9870198e2f7bb
Kali <kali@kali>
2022-06-29 16:33:34 +0100

    Updated files

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def islower(char):
+    if ((ord(char) >= 97) & (ord(char) <= 122)):
+        # print("{} => {}".format(char, ord(char)))
+        return (True)
+
+    return (False)
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(_str):
+    delta = ord('a') - ord('A')
+
+    for char in _str:
+        if ((ord(char) >= ord('a')) & (ord(char) <= ord('z'))):
+            print("{}".format(chr(ord(char) - delta)), end="")
+        else:
+            print("{}".format(char), end="")
+
+    print("", end="\n")
+
