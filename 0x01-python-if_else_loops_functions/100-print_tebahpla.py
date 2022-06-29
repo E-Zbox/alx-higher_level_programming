2e6820a5d6fc48793c50ecbe8da688e42eb46cfc
E-Zbox <zboxstudios97@gmail.com>
2022-06-29 23:26:29 +0100

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for count in range(ord('z'), ord('a') - 1, -1):
-    print(chr(count) if (count % 2 == 0) else chr(count - 32), end="")
+    print("{}".format(chr(count) if (count % 2 == 0) else chr(count - 32)), end="")
