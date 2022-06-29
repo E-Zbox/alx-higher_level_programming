dde445b1794af969aa3fdbcc033ec25485ef0810
E-Zbox <zboxstudios97@gmail.com>
2022-06-29 19:45:50 +0100

    correct output; pycodstyle validation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
         print("{:d}{:d}".format(num1, num2), end="")
-        print(", ", end="") if num1 != (10 - 1) else print("\n")
+        print(", ", end="") if num1 != (10 - 2) else print("\n")
 
