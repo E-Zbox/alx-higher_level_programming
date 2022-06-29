34fb10317d358b8dbd9cc28814043c7b606fe9e3
E-Zbox <zboxstudios97@gmail.com>
2022-06-29 18:53:16 +0100

    Fixed indentation

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-	text = "{:d} is zero".format(number)
+    text = "{:d} is zero".format(number)
 elif (number < 0):
-	text = "{:d} is negative".format(number)
+    text = "{:d} is negative".format(number)
 else:
-	text = "{:d} is positive".format(number)
+    text = "{:d} is positive".format(number)
 
 print(text)
