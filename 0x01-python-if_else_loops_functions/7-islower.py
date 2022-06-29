d5353a24868a75f095dba13659387372690479e8
E-Zbox <zboxstudios97@gmail.com>
2022-06-29 19:56:29 +0100

    7. islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(char):
     if ((ord(char) >= 97) & (ord(char) <= 122)):
-        # print("{} => {}".format(char, ord(char)))
+        ## print("{} => {}".format(char, ord(char)))
         return (True)
 
     return (False)
