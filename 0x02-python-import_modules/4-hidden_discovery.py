9c28a413cbcb017a3b31417f5661f32619244dba
E-Zbox <zboxstudios97@gmail.com>
2022-07-01 00:48:19 +0100

    4. who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+def read_hidden():
+    for prop in dir(hidden_4):
+        if prop[:2] != "__":
+            print(prop)
+
+if __name__ == "__main__":
+    read_hidden()
