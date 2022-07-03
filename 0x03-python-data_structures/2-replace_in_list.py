915c78b864f6e40e835ae45e704e93c67eb91b72
E-Zbox <zboxstudios97@gmail.com>
2022-07-03 20:21:57 +0100

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     len_my_list = len(my_list)
-    if (idx < 0 | idx >= len_my_list):
+    if ((idx < 0) or (idx >= len_my_list)):
         return (my_list)
 
     my_list[idx] = element
