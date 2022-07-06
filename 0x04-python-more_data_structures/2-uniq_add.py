556252cc773366726be50642895a839c5790b446
E-Zbox <zboxstudios97@gmail.com>
2022-07-07 00:40:13 +0100

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-from functools import reduce
-
 
 def uniq_add(my_list=[]):
     uniq_list = list(set(my_list))
-    return (reduce(lambda x, y: x + y, uniq_list))
+    return (sum(uniq_list))
