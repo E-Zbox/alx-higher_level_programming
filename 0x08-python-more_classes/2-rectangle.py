3573fdecc1c660544d5f4bcc46d1e3de3481e141
E-Zbox <zboxstudios97@gmail.com>
2022-07-25 23:29:18 +0100

    2. Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,4 +53,6 @@ class Rectangle:
             product of 2 and the sum of width and height
 
         """
+        if ((self.__width == 0) | (self.__height == 0)):
+            return (0)
         return (2 * (self.__width + self.__height))
