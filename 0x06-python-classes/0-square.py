b0c236fc51fe0107b11aefd6ad9bf8f9fea15529
E-Zbox <zboxstudios97@gmail.com>
2022-07-25 11:26:41 +0100

    0. My first Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""The Square module
+
+This module demonstrates creating a Square class and attributes
+
+File calling:
+    $ python3 0-square.py
+"""
+
 
 class Square:
-    """Class Square
-    """
+    """creates an empty Square class defining a class"""
+    pass
