c046f5773d4db5613bd578546c0d73314e163d06
E-Zbox <zboxstudios97@gmail.com>
2022-07-28 03:23:37 +0100

    Updated file content

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
         """ instantiates height property """
         if (type(value) is not int):
             raise TypeError("height must be an integer")
-        if (height < 0):
+        if (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = height
 
